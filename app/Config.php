a07c76d2942ccf7ff7f245510f61dc30b23b6c93
Erivks <mordecai.es27@gmail.com>
2020-02-18 15:50:05 -0300

    Definindo constantes

--- app/Config.php
+++ app/Config.php
@@ -1,3 +1,8 @@
 <?php 
 
+#Defining constants
 define('URL_BASE', 'localhost:8000');
+define('HOST_DATABASE', 'localhost');
+define('DB_NAME', 'laravel');
+define('USER_DATABASE', 'root');
+define('PASSWORD_DATABASE', '');
