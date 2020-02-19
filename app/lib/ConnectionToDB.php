7d2f541e878fc0d0e674c6ee23809a160c7c69ee
Erivks <mordecai.es27@gmail.com>
2020-02-19 12:10:59 -0300

    Documentando código

--- app/lib/ConnectionToDB.php
+++ app/lib/ConnectionToDB.php
@@ -4,18 +4,30 @@ namespace App\lib;
 
 use PDO;
 
+/*
+* Class to make connection with database
+*
+* @author Erick O. dos Santos
+* @access public
+*/
 abstract class ConnectionToDB
 {
     public static $connectionData;
 
+    /*
+    * Method to make a connection with database if its not exists
+    *
+    * @return PDO
+    * @throw \Exception When the connection fails
+    */
     public static function makeConnection()
     {
         if (self::$connectionData == null){
             try {
                 self::$connectionData = new PDO(
-                    'mysql: host='. HOST_DATABASE .'; dbname='. DB_NAME .'; charset=utf8', 
-                    USER_DATABASE,
-                    PASSWORD_DATABASE
+                    'mysql: host='. DATABASE_DATA['HOST'] .'; dbname='. DATABASE_DATA['DB_NAME'] .'; charset=utf8', 
+                    DATABASE_DATA['USER'],
+                    DATABASE_DATA['PASSWORD']
                 );
             } catch (Exception $error) {
                 throw new Exception("Não foi possível se conectar ao banco");
@@ -24,6 +36,11 @@ abstract class ConnectionToDB
         return self::$connectionData;
     }
 
+    /*
+    * Method to get connection already done
+    *
+    * @return PDO
+    */
     public static function getConnection()
     {
         return self::$connectionData;
