fffa69e1dfdcaeb407266a7622e71552c7ed7adb
Erivks <mordecai.es27@gmail.com>
2020-02-20 16:21:05 -0300

    Adição de model de usuário

---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use App\lib\ConnectionToDB;
+
+/*
+* Class for management and return user data from database
+*
+* @author Erick O. dos Santos
+* @access public
+*/
+class User
+{
+    /*
+    * Method for return a specific access from database by id
+    *
+    * @param string $id
+    * @return Array[] App\Models\User
+    */
+    public function getUserById($id)
+    {
+
+    }
+
+    /*
+    * Method for return all users from database
+    *
+    * @return App\Models\Access
+    */
+    public function getAllUsers()
+    {
+
+    }
+
+    /*
+    * Method for store user data in database
+    *
+    * @param Array[] $data
+    * @return void
+    */
+    public function storeUser($data)
+    {
+
+    }
+
+    /*
+    * Method for update a specific user data in database by id
+    *
+    * @param Array[] $data
+    * @param string $id
+    * @return void
+    */
+    public function updateUser($id, $data)
+    {
+
+    }
+
+    /*
+    * Method for delete a specific user data in database by id
+    *
+    * @param string $id
+    * @return void
+    */
+    public function deleteUser($id)
+    {
+
+    }
+}
