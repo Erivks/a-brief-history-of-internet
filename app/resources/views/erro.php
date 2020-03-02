d447654138e1c77c2faf08c61540c71b2c0360c0
Erivks <mordecai.es27@gmail.com>
2020-03-02 14:27:54 -0300

    Página de erros

---
+++ app/resources/views/erro.php
@@ -0,0 +1,6 @@
+<?= $this->layout('_layout') ?>
+
+</br>
+</br>
+<h1 class="text-center" style="display: block;">Ops! Desculpe, ocorreu um erro inesperado.</h1>
+<h2 class="text-center" style="display: block;">Código: <?= $this->e($errcode) ?></h2>
